bc47c898cf90f5baa03335ec3fbc0b88fd12a544
David Owens <david@owensd.io>
2015-01-28 22:20:51 -0800

    Day004: animating the back buffer.

--- src/macosx/macosx_handmade.swift
+++ src/macosx/macosx_handmade.swift
@@ -27,11 +27,31 @@ class GraphicsBufferView : NSView {
     let bitsPerPixel: UInt = 32
     let bytesPerPixel: UInt = 4
     
+    var offsetX = 0
+    var offsetY = 0
+    
     let colorSpace = CGColorSpaceCreateDeviceRGB()
-    let bitmapInfo = CGBitmapInfo(CGImageAlphaInfo.PremultipliedFirst.rawValue)
+    let bitmapInfo = CGBitmapInfo.ByteOrderDefault
+    
+    func timerUpdate() {
+        renderWeirdGradient(offsetX, offsetY)
+        
+        offsetX = offsetX &+ 1
+        offsetY = offsetY &+ 2
+    }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+
+        // TODO(owensd): This is a pretty hacky way to set this up, and is a bit
+        // slower than I had anticipated. However, I'm on a retina display, so that's
+        // a whole lot more pixels being processed as well...
+        let timer = NSTimer.scheduledTimerWithTimeInterval(
+            1.0/60.0,
+            target: self,
+            selector: Selector("timerUpdate"),
+            userInfo: nil,
+            repeats: true)
         
         updateBuffer(self.bounds.width, self.bounds.height)
     }
@@ -49,7 +69,27 @@ class GraphicsBufferView : NSView {
             bitmapInfo,
             provider, nil, true, kCGRenderingIntentDefault)
 
-        CGContextDrawImage(context, CGRect(x: 0, y: 0, width: Int(bufferWidth), height: Int(bufferHeight)), image)
+        let rect = CGRect(x: 0, y: 0, width: Int(bufferWidth), height: Int(bufferHeight))
+        CGContextDrawImage(context, rect, image)
+    }
+
+    func renderWeirdGradient(blueOffset: Int, _ greenOffset: Int) {
+        let pixels = UnsafeMutablePointer<UInt32>(buffer)
+        
+        for var y: UInt = 0; y < bufferHeight; y++ {
+            let row = y * bufferWidth
+            for var x: UInt = 0; x < bufferWidth; x++ {
+                let red = UInt32(0)
+                let green = UInt32(y + greenOffset)
+                let blue = UInt32(x + blueOffset)
+                let alpha = UInt32(0)
+                
+                pixels[Int(row + x)] = UInt32(alpha << 24 | blue << 16 | green << 8 | red)
+                
+            }
+        }
+        
+        self.needsDisplay = true
     }
 }
 
@@ -65,6 +105,8 @@ extension GraphicsBufferView: NSWindowDelegate {
         
         buffer = UnsafeMutablePointer<UInt8>.alloc(Int(bufferWidth * bufferHeight * bytesPerPixel))
         memset(buffer, 255, bufferWidth * bufferHeight * bytesPerPixel)
+        
+        renderWeirdGradient(0, 0)
     }
     
     func windowDidResize(notification: NSNotification) {
